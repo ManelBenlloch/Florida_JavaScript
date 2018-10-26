f5297a7fdbb06c1f1c2cf9933ba91772adfd7642
ManelBenlloch <32683873+ManelBenlloch@users.noreply.github.com>
2018-10-26 14:15:23 +0200

    Update Imprime palabras mayores a 3 Vocales.js

--- Imprime palabras mayores a 3 Vocales.js
+++ Imprime palabras mayores a 3 Vocales.js
@@ -1,51 +1,42 @@
 // Author: Manel Benlloch - 15/10/2018
 // Windows CMD Spanish Configuration, command: chcp 65001
-
-/*
-Realiza un programa que lea palabras por el teclado hasta que el usuario introduzca el carácter *.
- En ese momento, el programa imprimirá todas aquellas palabras que tenían al menos 3 vocales
-*/
-
 const rl = require("readline-sync");
-let palabras = [];
-let palabra = "";
-let a = 0; let e = 0; let i = 0; let o = 0; let u = 0;
-let result = "";
-let analytics = "";
 
+let items = [];
+let item = undefined;
+let contador = 1;
+let result = " ";
+console.log("<<<---Welcome to Shop List v1.00 --->>>");
+console.log("<<<---El programa termina cuando envias una respuesta vacia, para enviar pulsa Enter ⏎ . --->>>");
 do {
     do {
-        palabra = rl.question("Introduce una palabra: ").toLocaleLowerCase();
-    } while (palabra === "");
+        item = rl.question("Introduce el item " + contador + " :").toLocaleLowerCase();
+        if (item === "") {
+            item = "";
+            console.log("Has introducido una cadena vacia lo que significa que no hay nada mas que añadir.");
 
-    if (palabra === "*") {
-        break;
-    } else {
-        palabras.push(palabra);
+            break;
+        }
+        if (items.indexOf(item) > -1) {
+            console.log("El item esta repetido vuelve a introducir uno diferente.");
+        } else {
+            items.push(item);
+            console.log("Item agregado correctaente.");
+            contador++;
+        }
+    } while (items.indexOf(item) > -1);
+
+} while (item != "");
+
+// Mostrando datos
+if (items.length !== 0) {
+    console.log("\n Mostrando la lista de la compra... \n _____________________________________");
+    contador = 0;
+    for (let index = 0; index < items.length; index++) {
+        contador++;
+        console.log(" " + contador + ": " + items[index]);
     }
-
-} while (true);
-
-/* ARRIBA * PERFECTO */
-
-for (let value_i = 0; value_i <= palabras.length - 1; value_i++) {
-    analytics = palabras[value_i];
-    //console.log(palabras[value_i]);
-
-
-    for (x = 0; x <= analytics.length - 1; x++) {
-        if (analytics.charAt(x) === "a") { a = a + 1; }
-        if (analytics.charAt(x) === "e") { e = e + 1; }
-        if (analytics.charAt(x) === "i") { i = i + 1; }
-        if (analytics.charAt(x) === "o") { o = o + 1; }
-        if (analytics.charAt(x) === "u") { u = u + 1; }
-
-
-    }
-    result = (a + e + i + o + u);
-    if (result > 2) {
-        console.log(palabras[value_i]); result = 0;
-        a = 0; let e = 0; let i = 0; let o = 0; let u = 0;
-    }
-    a = 0; let e = 0; let i = 0; let o = 0; let u = 0;
+    console.log(" _____________________________________");
+} else {
+    console.log("\n <<-- La lista esta vacia, no hay nada que mostrar. -->");
 }
