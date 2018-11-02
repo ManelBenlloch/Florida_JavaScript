3e2be6ca813d7e60fa32e1bfefd5709dbb5c96fe
ManelBenlloch <32683873+ManelBenlloch@users.noreply.github.com>
2018-11-02 18:13:36 +0100

    Create Lista_De_Items_No_Repetido-V2.js

---
+++ Lista_De_Items_No_Repetido-V2.js
@@ -0,0 +1,121 @@
+// Author: Manel Benlloch - 15/10/2018
+// Windows CMD Spanish Configuration, command: chcp 65001
+const rl = require("readline-sync");
+
+let items = [];
+let item = undefined;
+let contador = 1;
+let result = " ";
+console.log("<<<---Welcome to Shop List v1.00 --->>>");
+console.log("<<<---El programa termina cuando envias una respuesta vacia, para enviar pulsa Enter ⏎ . --->>>");
+do {
+    do {
+        let repetido=false;
+        item = rl.question("Introduce el item " + contador + " :").toLocaleLowerCase();
+        if (item === "") {
+            item = "";
+            console.log("Has introducido una cadena vacia lo que significa que no hay nada mas que añadir.");
+
+            break;
+        }
+        /*
+        if (items.indexOf(item) > -1) {
+            console.log("El item esta repetido vuelve a introducir uno diferente."); 
+        } else {
+            items.push(item);
+            console.log("Item agregado correctaente.");
+            contador++;
+        } */
+
+        for (let index = 0; index < items.length; index++) {
+            if(ite===items[index]){
+            console.log("El item esta repetido vuelve a introducir uno diferente.");
+            repetido=true;
+            }else{
+
+            items.push(item);
+            console.log("Item agregado correctaente.");
+            contador++;
+            repetido=false;
+
+            }
+            
+        }
+
+     /* while (items.indexOf(item) > -1); */
+    } while (repetido===true);
+
+} while (item != "");
+
+// Mostrando datos
+if (items.length !== 0) {
+    console.log("\n Mostrando la lista de la compra... \n _____________________________________");
+    contador = 0;
+    for (let index = 0; index < items.length; index++) {
+        contador++;
+        console.log(" " + contador + ": " + items[index]);
+    }
+    console.log(" _____________________________________");
+} else {
+    console.log("\n <<-- La lista esta vacia, no hay nada que mostrar. -->");
+}// Author: Manel Benlloch - 15/10/2018
+// Windows CMD Spanish Configuration, command: chcp 65001
+const rl = require("readline-sync");
+
+let items = [];
+let item = undefined;
+let contador = 1;
+let result = " ";
+console.log("<<<---Welcome to Shop List v1.00 --->>>");
+console.log("<<<---El programa termina cuando envias una respuesta vacia, para enviar pulsa Enter ⏎ . --->>>");
+do {
+    do {
+        let repetido=false;
+        item = rl.question("Introduce el item " + contador + " :").toLocaleLowerCase();
+        if (item === "") {
+            item = "";
+            console.log("Has introducido una cadena vacia lo que significa que no hay nada mas que añadir.");
+
+            break;
+        }
+        /*
+        if (items.indexOf(item) > -1) {
+            console.log("El item esta repetido vuelve a introducir uno diferente."); 
+        } else {
+            items.push(item);
+            console.log("Item agregado correctaente.");
+            contador++;
+        } */
+
+        for (let index = 0; index < items.length; index++) {
+            if(ite===items[index]){
+            console.log("El item esta repetido vuelve a introducir uno diferente.");
+            repetido=true;
+            }else{
+
+            items.push(item);
+            console.log("Item agregado correctaente.");
+            contador++;
+            repetido=false;
+
+            }
+            
+        }
+
+     /* while (items.indexOf(item) > -1); */
+    } while (repetido===true);
+
+} while (item != "");
+
+// Mostrando datos
+if (items.length !== 0) {
+    console.log("\n Mostrando la lista de la compra... \n _____________________________________");
+    contador = 0;
+    for (let index = 0; index < items.length; index++) {
+        contador++;
+        console.log(" " + contador + ": " + items[index]);
+    }
+    console.log(" _____________________________________");
+} else {
+    console.log("\n <<-- La lista esta vacia, no hay nada que mostrar. -->");
+}
