4cd04ebf4637fee158b921d0e5f459d4a0177349
ManelBenlloch <32683873+ManelBenlloch@users.noreply.github.com>
2018-11-28 14:37:05 +0100

    Create stringsConcatenated.js

---
+++ stringsConcatenated.js
@@ -0,0 +1,10 @@
+// Author: Manel Benlloch - 28/11/2018
+// Windows CMD Spanish Configuration, command: chcp 65001
+let stringsConcatenated="";
+    
+let str1 = "[1 ";let str2 = "2, ";let str3 = "3, ";
+let str4 = "4, ";let str5 = "5, ";let str6 = "6, ";
+let str7 = "7, ";let str8 = "8, ";let str9 = "9, ";
+                 let str10 = "10] ";
+stringsConcatenated=stringsConcatenated.concat(str1,str2,str3,str4,str5,str6,str7,str8,str9,str10);
+console.log(stringsConcatenated);
