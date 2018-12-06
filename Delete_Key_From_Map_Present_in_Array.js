7dbd2940d92045ecc6f7aa9b9c8e083df10dcc3d
ManelBenlloch <32683873+ManelBenlloch@users.noreply.github.com>
2018-12-06 09:12:09 +0100

    Update Delete_Key_From_Map_Present_in_Array.js

--- Delete_Key_From_Map_Present_in_Array.js
+++ Delete_Key_From_Map_Present_in_Array.js
@@ -1,11 +1,16 @@
+// Author: Manel Benlloch - 15/10/2018
+// Windows CMD Spanish Configuration, command: chcp 65001
+
 let finalmente = new Map();
 finalmente.set("Hola", 2);
 finalmente.set("Adios", 10);
 stopwords_es = ["Hola", "mira", "novoy"];
+
 for (let i = 0; i < stopwords_es.length; i++) {
   eliminar = stopwords_es[i];
   envato(eliminar);
 }
+
 function envato(eliminar) {
   for (var [key, value] of finalmente) {
     if (key == eliminar) {
